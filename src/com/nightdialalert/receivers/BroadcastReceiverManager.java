ce5ea374f980f99cb6da62dca153c512db8ecfc0
Nitin Jain <nitjain@gmail.com>
2015-08-25 11:16:29 -0700

    Added Copyright Header

--- src/com/nightdialalert/receivers/BroadcastReceiverManager.java
+++ src/com/nightdialalert/receivers/BroadcastReceiverManager.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015, NightDialAlert
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.nightdialalert.receivers;
 
 import java.util.concurrent.Executors;
@@ -11,16 +26,17 @@ import android.util.Log;
 public class BroadcastReceiverManager {
     private static final String TAG = BroadcastReceiverManager.class.getSimpleName();
     private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-    
+
     public static void enableBroadcastReceiverAfterDelay(final Context mContext, final ComponentName mReceiverComponent, int seconds) {
         scheduler.schedule(new Runnable() {
-                public void run() { 
-                    enableBroadcastReceiver(mContext, mReceiverComponent); 
+                @Override
+                public void run() {
+                    enableBroadcastReceiver(mContext, mReceiverComponent);
                }
             },
             seconds, java.util.concurrent.TimeUnit.SECONDS);
     }
-    
+
     public static void enableBroadcastReceiver(Context mContext, ComponentName mReceiverComponent) {
         if (!isReceiverEnabled(mContext, mReceiverComponent)) {
             Log.d(TAG, "Turning ON Receiver");
